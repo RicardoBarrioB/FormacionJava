e7a4a767b1059b75e24c6aa56ffcf32d873713ce
RicardoBarrioB <162453002+RicardoBarrioB@users.noreply.github.com>
2024-03-10 22:35:24 +0100

    Add files via upload

---
+++ Curso/A04_Geometria/src/modelo/Figura.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Figura {
+	
+	public double x;
+	public double y;
+	
+	public void asignarValores(double valX, double valY) {
+		x = valX;
+		y = valY;
+	}
+	
+	public double area() {
+		return 0;
+	}
+	
+}
